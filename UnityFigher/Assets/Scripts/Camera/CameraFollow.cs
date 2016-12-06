78073921c6b8035effecb71c45366fa9a356f38b
danielcy <448219947@qq.com>
2016-12-06 17:39:23 +0800

    Setup environment, player and the camera.

---
+++ UnityFigher/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform player;
+	public float posSmoothing = 5f;
+	public float rotSmoothing = 2f;
+	public float adjustDistance = 1f;
+
+	private LayerMask environmentMask;
+	private Vector3 preMousePosition;
+	private Vector3 lastPosition;
+
+	void Start ()
+	{
+		environmentMask = LayerMask.GetMask ("Environment");
+
+		Vector3 offset = -6 * player.forward;
+		offset.y = 2f;
+		transform.position = player.position + offset;
+	}
+
+	void FixedUpdate ()
+	{
+		SetPosition ();
+		SetRotation ();
+		preMousePosition = Input.mousePosition;
+	}
+
+	void SetPosition ()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit rayHit = new RaycastHit ();
+		if (Physics.Raycast (camRay, out rayHit, 100f, environmentMask)) {
+			Vector3 direction = (rayHit.point - player.position).normalized;
+			Vector3 offset = -6 * direction;
+			offset.y = 2f;
+			Vector3 position = player.position + offset;
+			if (Vector3.Distance (position, lastPosition) > adjustDistance) {
+				transform.position = Vector3.Lerp (transform.position, position, posSmoothing * Time.deltaTime);
+				lastPosition = position;
+			}
+		}
+	}
+
+	void SetRotation ()
+	{
+		Vector3 direction = (player.position - transform.position).normalized;
+		direction.y = 0f;
+		Quaternion rotation = Quaternion.LookRotation (direction);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, rotSmoothing * Time.deltaTime);
+	}
+
+	bool IsMouseMoving ()
+	{
+		if (Input.mousePosition == preMousePosition) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
---
+++ UnityFigher/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 5f;
+	public Transform camera;
+
+	private Animator anim;
+	private Rigidbody playerRigidbody;
+	void Awake ()
+	{
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Moving (moveHorizontal, moveVertical);
+		Animating (moveHorizontal, moveVertical);
+	}
+
+	void Moving (float moveHorizontal, float moveVertical)
+	{
+		Vector3 direction = transform.position- Camera.main.transform.position;
+		Vector3 normalZ = new Vector3 (0, 0, 1f);
+		direction.y = 0f;
+
+		Vector3 cameraForward = Vector3.Scale (Camera.main.transform.forward, new Vector3 (1f, 0f, 1f)).normalized;
+		Vector3 movement = (moveHorizontal * Camera.main.transform.right + moveVertical * cameraForward) * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+
+		if (movement != new Vector3 (0, 0, 0)) {
+			Quaternion rotation = Quaternion.LookRotation (movement);
+			playerRigidbody.MoveRotation (rotation);
+		}
+
+	}
+
+	void Animating (float moveHorizontal, float moveVertical)
+	{
+		if (moveVertical != 0 || moveHorizontal != 0) {
+			anim.SetBool ("IsMoving", true);
+			anim.SetFloat ("MoveBlend", moveHorizontal * moveVertical);
+		} else {
+			anim.SetBool ("IsMoving", false);
+		}
+	}
+}
